7355338580f4f1212b05a5bce26e46256dba9054
Md. Nahid Hasan Bappi <nahidbappi13@gmail.com>
2024-09-03 00:41:00 +0600

    created first js file with a javascript problem solving

---
+++ con1.js
@@ -0,0 +1,17 @@
+// Problem: 01-------------------
+
+// Free Drinks
+//     - Burger more than 500tk: free Coke
+//     - Else Coke: 30tk
+
+
+let burgerPrice = 650;
+
+if (burgerPrice > 500){
+    console.log('free coke');
+}
+else{
+    console.log('coke: 30tk');
+}
+
+
