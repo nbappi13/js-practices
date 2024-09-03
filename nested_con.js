0d516cd0bda040833d11b4e61c83f21da3dc0509
Md. Nahid Hasan Bappi <nahidbappi13@gmail.com>
2024-09-04 05:49:20 +0600

    some changes

--- nested_con.js
+++ nested_con.js
@@ -1,21 +1,21 @@
 // Your score and friend's score
-let yourScore = 85;
+let myScore = 85;
 let friendScore = 85;
 
 // Main condition based on your score
-if (yourScore > 80) {
-    console.log("You scored more than 80, let's check your friend's score.");
+if (myScore > 80) {
+    console.log("my score is more than 80, let's check my friend's score.");
 
     if (friendScore > 80) {
-        console.log("Your friend also scored more than 80, go for lunch!");
+        console.log("My friend also score more than 80, We will go for lunch!");
     } else if (friendScore >= 60 && friendScore <= 80) {
-        console.log("Your friend scored between 60 and 80, tell your friend, 'Good luck next time!'");
+        console.log("My friend score between 60 and 80, I will tell my friend, 'Good luck next time!'");
     } else if (friendScore >= 40 && friendScore < 60) {
-        console.log("Your friend scored between 40 and 60, keep your friend's message unseen.");
+        console.log("My friend score between 40 and 60, I will keep my friend's message unseen.");
     } else {
-        console.log("Your friend scored less than 40, block your friend.");
+        console.log("My friend score less than 40, I will block my friend.");
     }
 
 } else {
-    console.log("You scored less than 80, go home, sleep, and act sad.");
+    console.log("My score less than 80, I will go home, sleep, and act sad.");
 }
