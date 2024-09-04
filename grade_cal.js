1214e164cf5dc58e731e6f80916c70ed2c9a9782
Md. Nahid Hasan Bappi <nahidbappi13@gmail.com>
2024-09-04 17:40:32 +0600

    some changes

--- grade_cal.js
+++ grade_cal.js
@@ -1,3 +1,19 @@
+// ---------------Problem--------------------------
+
+// Grade Calculator
+
+//     Create a simple JavaScript program that takes a student's score as input and returns their corresponding grade based on the following grading scale:
+
+//         A: 90-100
+//         B: 80-89
+//         C: 70-79
+//         D: 60-69
+//         F: 0-59
+
+
+// ----------------Solution----------------------------
+
+
 // ----Input Values 
 
 let studentScore = 78;  
