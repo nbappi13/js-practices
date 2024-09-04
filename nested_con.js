c9f5edc6af2b966117915517df93f2f88cb13e89
Md. Nahid Hasan Bappi <nahidbappi13@gmail.com>
2024-09-04 17:32:11 +0600

    some changes

--- nested_con.js
+++ nested_con.js
@@ -1,14 +1,14 @@
-// Your score and friend's score
+// My score and friend's score
 let myScore = 85;
 let friendScore = 85;
 
-// Main condition based on your score
+// Main condition based on My score
 if (myScore > 80) {
     console.log("my score is more than 80, let's check my friend's score.");
 
     if (friendScore > 80) {
         console.log("My friend also score more than 80, We will go for lunch!");
-    } else if (friendScore >= 60 && friendScore <= 80) {
+    } else if (friendScore >= 60 && friendScore < 80) {
         console.log("My friend score between 60 and 80, I will tell my friend, 'Good luck next time!'");
     } else if (friendScore >= 40 && friendScore < 60) {
         console.log("My friend score between 40 and 60, I will keep my friend's message unseen.");
@@ -19,3 +19,9 @@ if (myScore > 80) {
 } else {
     console.log("My score less than 80, I will go home, sleep, and act sad.");
 }
+
+
+
+
+
+
